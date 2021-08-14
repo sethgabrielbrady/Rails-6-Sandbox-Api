5ad285173185cea817aadd92a8537c5a636b9adc
sethgabrielbrady <sethgabrielbrady@gmail.com>
2021-08-14 18:16:38 -0400

    Adds user model, jwt, and tests

---
+++ app/controllers/api/v1/authentication_controller.rb
@@ -0,0 +1,22 @@
+module Api
+  module V1
+    class AuthenticationController < ApplicationController
+      rescue_from ActionController::ParameterMissing, with: :parameter_missing
+
+      def create
+        p params.require(:password).inspect
+        user = User.find_by(username: params.require(:username))
+        token = AuthenticationTokenService.call(user.id)
+
+        render json: { token: token }, status: :created
+      end
+
+      private
+
+      def parameter_missing(e)
+        render json: { error: e.message }, status: :unprocessable_entity
+      end
+
+    end
+  end
+end
