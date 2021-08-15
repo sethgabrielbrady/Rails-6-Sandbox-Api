112d4c1231b23de9177fb168254d191fdfe2b9be
sethgabrielbrady <sethgabrielbrady@gmail.com>
2021-08-15 12:42:18 -0400

    Adds basic authentication using JWT and brcypt

--- spec/requests/authentication_spec.rb
+++ spec/requests/authentication_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 describe 'Authentication', type: :request do
   describe 'POST /authenticate' do
-    let!(:user) { FactoryBot.create(:user, username: 'Foobius66') }
+    let!(:user) { FactoryBot.create(:user, username: 'Foobius66', password: '123456') }
 
     it "authenticates the client" do
-      post '/api/v1/authenticate', params: { username: user.username, password: '123456' }
+      post '/api/v1/authenticate', params: { username: user.username, password: user.password }
 
       expect(response).to have_http_status(:created)
       expect(JSON.parse(response.body)).to eq({
-        'token' => '123'
+        'token' => 'eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxfQ.DiPWrOKsx3sPeVClrm_j07XNdSYHgBa3Qctosdxax3w'
       })
     end
 
@@ -24,12 +24,18 @@ describe 'Authentication', type: :request do
     end
 
     it 'returns error when password is missing' do
-      post '/api/v1/authenticate', params: { username: 'Foobius66' }
+      post '/api/v1/authenticate', params: { username: user.username }
 
       expect(response).to have_http_status(:unprocessable_entity)
       expect(JSON.parse(response.body)).to eq({
         'error' => 'param is missing or the value is empty: password'
       })
+    end
+
+    it 'returns error when password is incorrect' do
+      post '/api/v1/authenticate', params: { username: user.username, password: "incorrect" }
+
+      expect(response).to have_http_status(:unauthorized)
     end
   end
 end
